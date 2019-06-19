41a84ada21908dbafb3c9e0916f9c65dcdea00dd
Jiren Jin <jinjirenjjr@gmail.com>
2019-06-19 23:00:13 +0900

    Use functions instead of class methods

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,5 @@
 """Binary tree representation and common methods."""
+import collections
 
 
 class Node:
@@ -8,43 +9,35 @@ class Node:
         self.right = None
 
 
-class BinaryTree:
-    def __init__(self, root):
-        self.root = root
+def preorder_traverse(root, process):
+    """Traverse the tree in preorder and apply `process` function."""
+    if root:
+        process(root)
+        preorder_traverse(root.left, process)
+        preorder_traverse(root.right, process)
 
-    @staticmethod
-    def preorder_traverse(root, process):
-        """Traverse the tree in preorder and apply `process` function."""
-        if root:
-            process(root)
-            self.preorder_traverse(root.left, process)
-            self.preorder_traverse(root.right, process)
+def inorder_traverse(root, process):
+    """Traverse the tree in inorder and apply `process` function."""
+    if root:
+        inorder_traverse(root.left, process)
+        process(root)
+        inorder_traverse(root.right, process)
 
-    @staticmethod
-    def inorder_traverse(root, process):
-        """Traverse the tree in inorder and apply `process` function."""
-        if root:
-            self.inorder_traverse(root.left, process)
-            process(root)
-            self.inorder_traverse(root.right, process)
-    
-    @staticmethod
-    def postorder_traverse(root, process):
-        """Traverse the tree in postorder and apply `process` function."""
-        if root:
-            self.postorder_traverse(root.left, process)
-            self.postorder_traverse(root.right, process)
-            process(root)
+def postorder_traverse(root, process):
+    """Traverse the tree in postorder and apply `process` function."""
+    if root:
+        postorder_traverse(root.left, process)
+        postorder_traverse(root.right, process)
+        process(root)
 
 
-    @staticmethod
-    def bfs(root, process):
-        queue = collections.deque([root]):
-        while queue:
-            node = queue.popleft()
-            if node:
-                process(node)
-                if node.left:
-                    self.bfs(root.left, process)
-                if node.right:
-                    self.bfs(root.right, process)
+def bfs(root, process):
+    queue = collections.deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            process(node)
+            if node.left:
+                bfs(root.left, process)
+            if node.right:
+                bfs(root.right, process)
