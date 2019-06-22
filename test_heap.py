1ed4cd4bb979648863d938c719614d326c147d56
Jiren Jin <jinjirenjjr@gmail.com>
2019-06-22 15:45:30 +0900

    Update the unittest for heap

--- test_heap.py
+++ test_heap.py
@@ -4,20 +4,25 @@ import random
 import heap
 
 
-def test_heap():
+def test_heap_insert_update_pop_top():
+    """Test for insert, update_key, pop and top methods of the `Heap` class.
+
+    Note that `heapq` module will use the tuple (key, node) together as the
+    actual key, and we also need to use both as actual keys to make sure the
+    popped nodes are the same for the builtin and customized heap.
+    """
     heap_queue = heap.Heap()
     builtin_heap = []
     for node in range(10000):
         if heap_queue.is_empty():
             assert not builtin_heap 
         else:
-            assert heap_queue.top()[0] == builtin_heap[0][0]
+            # we compare the actual key used: (key, node) tuple
+            assert heap_queue.top()[0] == builtin_heap[0]
         key = random.randint(0, 1000)
-        heap_queue.insert(node, key)
+        heap_queue.insert(node, (key, node))
         heapq.heappush(builtin_heap, (key, node))
-        assert heap_queue.top()[0] == builtin_heap[0][0]
+        assert heap_queue.top()[0] == builtin_heap[0]
         if not heap_queue.is_empty():
             if random.random() > 0.5:
-                key1, _ = heap_queue.pop()
-                key2, _ = heapq.heappop(builtin_heap)
-                assert key1 == key2
+                assert heap_queue.pop()[0] == heapq.heappop(builtin_heap)
